a213521c92a47d70bcae9feb7c10d7f5eac342ca
Felipe Mignone <dev.felipemignone@gmail.com>
2026-02-17 02:34:11 -0300

    feat: create store and webhook domains

---
+++ src/main/java/com/api/marketplace/domain/model/store/Store.java
@@ -0,0 +1,37 @@
+package com.api.marketplace.domain.model.store;
+
+import java.util.UUID;
+
+public class Store {
+    private final Integer id;
+    private final UUID externalId;
+    private String name;
+    private final String cnpj;
+
+    public Store(Integer id, UUID externalId, String cnpj, String name) {
+        this.id = id;
+        this.externalId = externalId;
+        this.cnpj = cnpj;
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public UUID getExternalId() {
+        return externalId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/main/java/com/api/marketplace/domain/model/webhook/Webhook.java
@@ -0,0 +1,46 @@
+package com.api.marketplace.domain.model.webhook;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Webhook {
+    private final Integer id;
+    private String callbackUrl;
+    private final List<Integer> storeIds = new ArrayList<>();
+
+
+    public Webhook(Integer id, String callbackUrl) {
+        this.id = id;
+        this.callbackUrl = callbackUrl;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getCallbackUrl() {
+        return callbackUrl;
+    }
+
+    public List<Integer> getStoreIds() {
+        return storeIds;
+    }
+
+    public void setCallbackUrl(String callbackUrl) {
+        this.callbackUrl = callbackUrl;
+    }
+
+    public void addStoreId(Integer newStoreId) throws IllegalArgumentException {
+        if(storeIds.stream().anyMatch(id -> id.equals(newStoreId))){
+            throw new IllegalArgumentException("StoreId already added");
+        }
+        this.storeIds.add(newStoreId);
+    }
+
+    public void removeStoreId(Integer storeIdToRemove) throws IllegalArgumentException {
+        if(storeIds.stream().noneMatch(id -> id.equals(storeIdToRemove))){
+            throw new IllegalArgumentException("StoreId not found");
+        }
+        this.storeIds.remove(storeIdToRemove);
+    }
+}
